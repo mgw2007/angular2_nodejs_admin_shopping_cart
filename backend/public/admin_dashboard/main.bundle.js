6c4baf96a0203aa9475b4409a465d6babaffce35
mina <mina.iti2011@gmail.com>
2017-07-19 14:01:00 +0200

    login view

--- backend/public/admin_dashboard/main.bundle.js
+++ backend/public/admin_dashboard/main.bundle.js
@@ -902,15 +902,10 @@ LocalHelperService = __decorate([
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-// The file contents for the current environment will overwrite these during build.
 var environment = {
-    production: false,
-    localApiUrl: 'http://localhost:3000/admin',
-    localSiteUrl: 'http://localhost:3000/'
+    production: true,
+    localApiUrl: '/admin',
+    localSiteUrl: '/'
 };
 //# sourceMappingURL=environment.js.map
 
